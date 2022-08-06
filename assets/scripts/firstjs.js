d9416a54d0cbfe99b0a6afc6fbd4b1013af5a433
Didiertose <didiertose@gmail.com>
2022-08-06 12:09:41 +0200

    cvv

--- assets/scripts/firstjs.js
+++ assets/scripts/firstjs.js
@@ -1,5 +1,17 @@
-
+var corps = document.getElementsByTagName("body")[0];
 var date = new Date();
 var anneeBalise = document.querySelector(".annee_courant");
 anneeBalise.innerHTML = date.getFullYear();
+
+
+// dynamisation du menu
+var content  = document.querySelector(".content_click");
+function ouvreMenus() {
+    content.style.display="grid";
+    content.setAttribute("id", "repons");
+}
+function fermeMenus() {
+    content.style.display="none";
+    content.removeAttribute("id");
+}
 
